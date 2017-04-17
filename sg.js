21e63b8ee17912e1840e609b7564f3df015412a4
Spencer Michalke <spencer.j.michalke@gmail.com>
2017-04-17 22:44:43 +0000

    Object Falls

--- sg.js
+++ sg.js
@@ -16,5 +16,42 @@ $("#body").keydown(function(event) {
     }
 });
 
+var object = $(".fall");
+setInterval(function(){
+var bot = $(".fall").offset().top;
+var top1 = $("#box").offset().top - $(".fall").height();
+    if (bot < top1){
+        object.css("top", object.offset().top + 20);
+        //fix for falling object
+        
+    } else{
+        object.css("top", 0);
+    }    
+    
 
-});
+    }, 100);
+    });
+
+
+
+
+/** //sets element to the variable $pic
+var $pic = $("#pic"); 
+
+$pic.click(function(){
+    setInterval(function(){
+
+var bot = $("#pic").offset().top;
+var top1 = $("#ground").offset().top - $("#pic").height();
+        console.log(bot);
+        console.log(top1);
+    if (bot < top1){
+        $pic.css("top", $("#pic").offset().top + 20);
+    } else{
+        $pic.css("height", $("#pic").height() - 10);
+    }    
+    
+
+    }, 100);
+    });
+**/
