393fb1ae13dd9fdb9cc250a3c58c63808f9f4915
Spencer Michalke <spencer.j.michalke@gmail.com>
2017-04-26 22:37:27 +0000

    Update

--- sg.js
+++ sg.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 
+var score = 0;
+
 $("#body").keydown(function(event){
 var left = $("#box").offset().left
 var right = $("#p").offset().left
@@ -17,10 +19,10 @@ var right2 = $("#box").offset().left + $("#box").width()
         }
         else if (event.which === 39) {
             if(right2 < left2){
-                $("#box").css("left", $("#box").offset().left + 10);
+                $("#box").css("left", $("#box").offset().left + 15);
                 }
             else if (event.which === 37){
-            $("#box").css("left", $("#box").offset().left - 10);
+            $("#box").css("left", $("#box").offset().left - 15);
                 }
         }
         else {
@@ -35,13 +37,19 @@ var bot = $(".fall").offset().top;
 var top1 = $("#box").offset().top - $(".fall").height();
     
     if (bot < top1){
-        object.css("top", object.offset().top + 20);
+        object.css("top", object.offset().top + 10);
         //fix for falling object
     } else{
-        
         object.css("left", rando);
         object.css("top", 0);
     }    
     
+var botdiv = $(".fall").offset().top + $(".fall").height();
+var top = $("#box").offset().top;
+
+    if (botdiv >= top){
+        score += 1;
+        $("#total").html("Score: " + score);
+    }
     }, 100);
 });
